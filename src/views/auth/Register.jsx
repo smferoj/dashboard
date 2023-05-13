c1b860c573ed4a966c193ef88bf7151799747269
SM Feroj <sm.feroj@gmail.com>
2023-05-13 09:00:44 +0600

    register page

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import {AiOutlineGooglePlus, AiOutlineGithub} from 'react-icons/ai';
+import {FiFacebook} from 'react-icons/fi';
+import {CiTwitter} from 'react-icons/ci';
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='min-w-screen min-h-screen bg-[#161d31] flex justify-center items-center'>
+       <div className='w-[350px] text-[#d0d2d6] p-2'>
+        <div className='bg-[#282046] p-4 rounded-md'>
+          <h2 className='text-xl mb-3'> Welcome to E-Commerce</h2>
+            <p className='text-sm mb-3'> Please register to your account and start business</p>
+            <form>
+           <div className='flex flex-col w-full gap-1 mb-3'>
+            <label htmlFor="name">Name</label>
+            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="text" name='name' placeholder='name' id='name' required/>
+           </div>
+
+           <div className='flex flex-col w-full gap-1 mb-3'>
+            <label htmlFor="email">Email</label>
+            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="text" name='email' placeholder='email' id='email' required/>
+           </div>
+            
+
+           <div className='flex flex-col w-full gap-1 mb-3'>
+            <label htmlFor="password">Password</label>
+            <input className='px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-[#d0d2d6] focus:border-indigo-500 overflow-hidden' type="password" name='password' placeholder='password' id='password' required/>
+           </div>
+            
+           <div className='flex items-center  w-full gap-3 mb-3'>
+            <input className='w-4 h-4 text-blue-600 overflow-hidden bg-gray-100 rounded border-gray-300 foucus:ring-blue-500' type="checkbox" name='checkbox' id='checkbox' required/>
+            <label htmlFor="checkbox">I agree to privacy policy and terms</label>
+           </div>
+            
+             <button className='bg-blue-500 w-full hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3 '> Sign Up</button>
+            
+            <div flex items-center mb-3 gap-3 jusitfy-center>
+              <p>Already have an account ? <Link to ="/login"> Signin here</Link> </p>
+            </div>
+
+            <div className='w-full flex justify-center items-center mb-3'>
+              <div className='w-[45%] bg-slate-700 h-[1px]'></div>
+                <div className='w-[10%] flex justify-center items-center'>
+                <span className='pb-1'>Or</span>
+                </div>
+              <div className='w-[45%] bg-slate-700 h-[1px]'></div>
+            </div>
+              <div className='flex justify-center items-center gap-3'>
+              <div className='w-[35px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                <span> <AiOutlineGooglePlus/></span>
+              </div>
+
+              <div className='w-[35px] h-[35px] flex rounded-md bg-indigo-700 shadow-lg hover:shadow-indigo-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                <span> <FiFacebook/></span>
+              </div>
+
+              <div className='w-[35px] h-[35px] flex rounded-md bg-cyan-700 shadow-lg hover:shadow-cyan-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                <span> <CiTwitter/></span>
+              </div>
+
+              <div className='w-[35px] h-[35px] flex rounded-md bg-purple-700 shadow-lg hover:shadow-purple-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                <span> <AiOutlineGithub/></span>
+              </div>
+
+              </div>
+             
+            </form>
+        </div>
+       </div>
+      
+      </div>
   )
 }
 
